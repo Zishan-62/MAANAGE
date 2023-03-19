d03254b3a610860c68d67435c79ecd667f0b593e
UMARSIDDD <umarsidd098@gmail.com>
2023-03-19 19:28:49 +0530

    project label added

--- mini_project_3/maanage/lib/Project/projects.dart
+++ mini_project_3/maanage/lib/Project/projects.dart
@@ -12,6 +12,65 @@ class ProjectPage extends StatefulWidget {
 class _ProjectPageState extends State<ProjectPage> {
   @override
   Widget build(BuildContext context) {
+    final label = [
+      Align(
+        alignment: Alignment.center,
+        child: Container(
+          padding: EdgeInsets.all(5),
+          alignment: Alignment.center,
+          width: MediaQuery.of(context).size.width * 0.3,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(10),
+              bottomRight: Radius.circular(10),
+            ),
+            color: Color(0xffEA4335),
+          ),
+          child: Text(
+            "URGENT",
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+        ),
+      ),
+      Align(
+        alignment: Alignment.center,
+        child: Container(
+          padding: EdgeInsets.all(5),
+          width: MediaQuery.of(context).size.width * 0.6,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(10),
+              bottomRight: Radius.circular(10),
+            ),
+            color: Color(0xffFBBC05),
+          ),
+          child: Text(
+            "IMMEDIATE ATTENTION",
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+        ),
+      ),
+      Align(
+        alignment: Alignment.center,
+        child: Container(
+          padding: EdgeInsets.all(5),
+          width: MediaQuery.of(context).size.width * 0.3,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(10),
+              bottomRight: Radius.circular(10),
+            ),
+            color: Color(0xff4CB151),
+          ),
+          child: Text(
+            "Done",
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+        ),
+      ),
+    ];
     return Scaffold(
       appBar: AppBar(
         leading: Image.asset(
@@ -35,10 +94,10 @@ class _ProjectPageState extends State<ProjectPage> {
           )
         ],
       ),
-      body: ListView(
-        children: [
-          for (var i = 0; i < 10; i++)
-            Padding(
+      body: ListView.builder(
+          itemCount: 3,
+          itemBuilder: ((context, index) {
+            return Padding(
               padding: EdgeInsets.symmetric(
                   horizontal: MediaQuery.of(context).size.width * 0.04,
                   vertical: MediaQuery.of(context).size.width * 0.03),
@@ -60,26 +119,27 @@ class _ProjectPageState extends State<ProjectPage> {
                         padding: const EdgeInsets.all(8.0),
                         child: Column(
                           children: [
-                            Align(
-                              alignment: Alignment.center,
-                              child: Container(
-                                alignment: Alignment.center,
-                                width: 100,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.only(
-                                    bottomLeft: Radius.circular(10),
-                                    bottomRight: Radius.circular(10),
-                                  ),
-                                  color: Color(0xffEA4335),
-                                ),
-                                child: Text(
-                                  "URGENT",
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white),
-                                ),
-                              ),
-                            ),
+                            label[index],
+                            // Align(
+                            //   alignment: Alignment.center,
+                            //   child: Container(
+                            //     alignment: Alignment.center,
+                            //     width: 100,
+                            //     decoration: BoxDecoration(
+                            //       borderRadius: BorderRadius.only(
+                            //         bottomLeft: Radius.circular(10),
+                            //         bottomRight: Radius.circular(10),
+                            //       ),
+                            //       color: Color(0xffEA4335),
+                            //     ),
+                            //     child: Text(
+                            //       "URGENT",
+                            //       style: TextStyle(
+                            //           fontWeight: FontWeight.bold,
+                            //           color: Colors.white),
+                            //     ),
+                            //   ),
+                            // ),
                             Row(
                               // mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: [
@@ -141,9 +201,117 @@ class _ProjectPageState extends State<ProjectPage> {
                   ],
                 ),
               ),
-            )
-        ],
-      ),
+            );
+          })),
+      // body: ListView(
+      //   children: [
+      //     for (var i = 0; i < 10; i++)
+      //       Padding(
+      //         padding: EdgeInsets.symmetric(
+      //             horizontal: MediaQuery.of(context).size.width * 0.04,
+      //             vertical: MediaQuery.of(context).size.width * 0.03),
+      //         child: Container(
+      //           child: Column(
+      //             children: [
+      //               Align(
+      //                   alignment: Alignment.topLeft,
+      //                   child: Text(
+      //                     "20/11/122",
+      //                     style: TextStyle(fontWeight: FontWeight.bold),
+      //                   )),
+      //               Card(
+      //                 shape: RoundedRectangleBorder(
+      //                   borderRadius: BorderRadius.circular(20),
+      //                 ),
+      //                 color: Color(0xFFF4f4f4),
+      //                 child: Padding(
+      //                   padding: const EdgeInsets.all(8.0),
+      //                   child: Column(
+      //                     children: [
+      //                       Align(
+      //                         alignment: Alignment.center,
+      //                         child: Container(
+      //                           alignment: Alignment.center,
+      //                           width: 100,
+      //                           decoration: BoxDecoration(
+      //                             borderRadius: BorderRadius.only(
+      //                               bottomLeft: Radius.circular(10),
+      //                               bottomRight: Radius.circular(10),
+      //                             ),
+      //                             color: Color(0xffEA4335),
+      //                           ),
+      //                           child: Text(
+      //                             "URGENT",
+      //                             style: TextStyle(
+      //                                 fontWeight: FontWeight.bold,
+      //                                 color: Colors.white),
+      //                           ),
+      //                         ),
+      //                       ),
+      //                       Row(
+      //                         // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      //                         children: [
+      //                           Image.asset('assets/images/office.png'),
+      //                           SizedBox(
+      //                             width:
+      //                                 MediaQuery.of(context).size.width * 0.1,
+      //                           ),
+      //                           Text(
+      //                             "Goal",
+      //                             style: TextStyle(
+      //                                 fontWeight: FontWeight.bold,
+      //                                 fontFamily: "Montserrat",
+      //                                 color: Color(0xFF3C5BFA)),
+      //                           ),
+      //                           SizedBox(
+      //                             width:
+      //                                 MediaQuery.of(context).size.width * 0.4,
+      //                           ),
+      //                           IconButton(
+      //                             onPressed: () {},
+      //                             icon: Icon(Icons.more_vert),
+      //                             color: Color(0xFF3C5BFA),
+      //                           )
+      //                         ],
+      //                       ),
+      //                       SizedBox(
+      //                         height: MediaQuery.of(context).size.height * 0.03,
+      //                       ),
+      //                       Row(
+      //                         children: [
+      //                           //niche call kiya hooo
+      //                           hello(),
+      //                           SizedBox(
+      //                             width:
+      //                                 MediaQuery.of(context).size.width * 0.1,
+      //                           ),
+      //                           InkWell(
+      //                             onTap: () {},
+      //                             child: Image.asset("assets/images/link.png"),
+      //                           ),
+      //                           InkWell(
+      //                             onTap: () {},
+      //                             child:
+      //                                 Image.asset("assets/images/massage.png"),
+      //                           ),
+      //                           InkWell(
+      //                             onTap: () {
+      //                               Navigator.pushNamed(context, "viewproject");
+      //                             },
+      //                             child: Image.asset("assets/images/done.png"),
+      //                           )
+      //                         ],
+      //                       )
+      //                     ],
+      //                   ),
+      //                 ),
+      //               )
+      //             ],
+      //           ),
+      //         ),
+      //       )
+      //   ],
+      // ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Color(0xFF3C5BFA),
         onPressed: () {
